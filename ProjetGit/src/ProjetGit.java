25847c3623781a2b982926e53336f05e45d6c541
Stargamer10 <2237610@cegeplimoilou.ca>
2023-02-20 13:14:03 -0500

    Ajout de la méthode faireUnTest()

--- ProjetGit/src/ProjetGit.java
+++ ProjetGit/src/ProjetGit.java
@@ -1,9 +1,13 @@
 public class ProjetGit {
     public ProjetGit() {
+        faireUnTest();
+    }
+
+    private void faireUnTest() {
+        System.out.println("Ceci est un test");
     }
 
     public static void main(String[] args) {
-
-        
+        new ProjetGit();
     }
 }
